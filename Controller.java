16735abf59188877bf64ffc9c7f438feb6741159
Bet <145491864+BetHdez@users.noreply.github.com>
2024-05-08 23:52:51 -0600

    Update and rename AController.java to Controller.java

---
+++ Controller.java
@@ -0,0 +1,25 @@
+import com.fasterxml.jackson.databind.JsonNode;
+/**
+ *
+ * @author Bet Hdz
+ */
+public class Controller {
+    
+    private Model model;
+    private View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void finalPerfiles() {
+        try {
+            JsonNode perfiles = model.obtenerPerfiles();
+            view.mostrarPerfiles(perfiles);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+    
